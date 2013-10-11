jowinter/qemu-trustzone

target-arm: Prepare emulation of TrustZone normal/secure world MMU support.
This patch adds the core support infrastructure required to emulate
the secure and normal world MMU views of a TrustZone system. This patch
comprises the following (atomically) related changes:

* Introduce two additional ARM MMU modes for use by the normal world
  kernel/user-mode simulation.

* Add secure/normal world banks of MMU related CP15 registers.
  (TTBCR, TTBR0/1, IFAR, DFAR, DFSR, IFSR, PA)

  Note that the banked nature of these registers only applies to
  systems with an MMU (VMSA). Microcontrollers with an MPU (PMSAv5)
  only use the "secure" half of the banked c5_data/c5_insn registers
  to encode their memory access permissions.

* Adapt the MMU translation logic in get_phys_addr() to honor the
  security state of the processor core. A new "is_secure" parameter
  selects the CP15 register bank to be used for translation. Support
  for the VMSAv6/v7 "NS" bit in section translations will be added
  in a later patch of this series.

  The mmu_idx parameter is used in cpu_arm_handle_mmu_fault() to
  determine the world causing an MMU fault. Debug access in
  cpu_get_phys_page_debug() uses the arm_current_secure() function to
  determine the current world for translating MMU requests.

Note that this patch does not wire-up the new normal world MMU modes
in translate.c, in order to isolate infrastructure changes from binary
translator changes. The existing binary translator is compatible with
this patch for secure-world only guests.

target-arm/machine.c
@@ -136,7 +136,20 @@ static const VMStateDescription vmstate_normal_world = {
     .minimum_version_id = 1,
     .minimum_version_id_old = 1,
     .fields = (VMStateField[]) {
-        VMSTATE_UINT32(env.cp15.c1_sys.normal, ARMCPU),
+        VMSTATE_UINT32(env.cp15.c2_base0.normal, ARMCPU),
+        VMSTATE_UINT32(env.cp15.c2_base0_hi.normal, ARMCPU),
+        VMSTATE_UINT32(env.cp15.c2_base1.normal, ARMCPU),
+        VMSTATE_UINT32(env.cp15.c2_base1_hi.normal, ARMCPU),
+        VMSTATE_UINT32(env.cp15.c2_control.normal, ARMCPU),
+        VMSTATE_UINT32(env.cp15.c2_mask.normal, ARMCPU),
+        VMSTATE_UINT32(env.cp15.c2_base_mask.normal, ARMCPU),
+        VMSTATE_UINT32(env.cp15.c3.normal, ARMCPU),
+        VMSTATE_UINT32(env.cp15.c5_insn.normal, ARMCPU),
+        VMSTATE_UINT32(env.cp15.c5_data.normal, ARMCPU),
+        VMSTATE_UINT32(env.cp15.c6_insn.normal, ARMCPU),
+        VMSTATE_UINT32(env.cp15.c6_data.normal, ARMCPU),
+        VMSTATE_UINT32(env.cp15.c7_par.normal, ARMCPU),
+        VMSTATE_UINT32(env.cp15.c7_par_hi.normal, ARMCPU),
         VMSTATE_UINT32(env.cp15.c12_vbar.normal, ARMCPU),
         VMSTATE_UINT32(env.cp15.c13_context.normal, ARMCPU),
         VMSTATE_UINT32(env.cp15.c13_fcse.normal, ARMCPU),
@@ -203,23 +216,23 @@ const VMStateDescription vmstate_arm_cpu = {
         VMSTATE_UINT32(env.cp15.c1_scr, ARMCPU),
         VMSTATE_UINT32(env.cp15.c1_sedbg, ARMCPU),
         VMSTATE_UINT32(env.cp15.c1_nseac, ARMCPU),
-        VMSTATE_UINT32(env.cp15.c2_base0, ARMCPU),
-        VMSTATE_UINT32(env.cp15.c2_base0_hi, ARMCPU),
-        VMSTATE_UINT32(env.cp15.c2_base1, ARMCPU),
-        VMSTATE_UINT32(env.cp15.c2_base1_hi, ARMCPU),
-        VMSTATE_UINT32(env.cp15.c2_control, ARMCPU),
-        VMSTATE_UINT32(env.cp15.c2_mask, ARMCPU),
-        VMSTATE_UINT32(env.cp15.c2_base_mask, ARMCPU),
+        VMSTATE_UINT32(env.cp15.c2_base0.secure, ARMCPU),
+        VMSTATE_UINT32(env.cp15.c2_base0_hi.secure, ARMCPU),
+        VMSTATE_UINT32(env.cp15.c2_base1.secure, ARMCPU),
+        VMSTATE_UINT32(env.cp15.c2_base1_hi.secure, ARMCPU),
+        VMSTATE_UINT32(env.cp15.c2_control.secure, ARMCPU),
+        VMSTATE_UINT32(env.cp15.c2_mask.secure, ARMCPU),
+        VMSTATE_UINT32(env.cp15.c2_base_mask.secure, ARMCPU),
         VMSTATE_UINT32(env.cp15.c2_data, ARMCPU),
         VMSTATE_UINT32(env.cp15.c2_insn, ARMCPU),
-        VMSTATE_UINT32(env.cp15.c3, ARMCPU),
-        VMSTATE_UINT32(env.cp15.c5_insn, ARMCPU),
-        VMSTATE_UINT32(env.cp15.c5_data, ARMCPU),
+        VMSTATE_UINT32(env.cp15.c3.secure, ARMCPU),
+        VMSTATE_UINT32(env.cp15.c5_insn.secure, ARMCPU),
+        VMSTATE_UINT32(env.cp15.c5_data.secure, ARMCPU),
         VMSTATE_UINT32_ARRAY(env.cp15.c6_region, ARMCPU, 8),
-        VMSTATE_UINT32(env.cp15.c6_insn, ARMCPU),
-        VMSTATE_UINT32(env.cp15.c6_data, ARMCPU),
-        VMSTATE_UINT32(env.cp15.c7_par, ARMCPU),
-        VMSTATE_UINT32(env.cp15.c7_par_hi, ARMCPU),
+        VMSTATE_UINT32(env.cp15.c6_insn.secure, ARMCPU),
+        VMSTATE_UINT32(env.cp15.c6_data.secure, ARMCPU),
+        VMSTATE_UINT32(env.cp15.c7_par.secure, ARMCPU),
+        VMSTATE_UINT32(env.cp15.c7_par_hi.secure, ARMCPU),
         VMSTATE_UINT32(env.cp15.c9_insn, ARMCPU),
         VMSTATE_UINT32(env.cp15.c9_data, ARMCPU),
         VMSTATE_UINT32(env.cp15.c9_pmcr, ARMCPU),

CodeLinaro-mirror/la_trusty_external_qemu

target-arm/machine.c
@@ -138,7 +138,21 @@ static const VMStateDescription vmstate_normal_world = {
     .minimum_version_id = 1,
     .minimum_version_id_old = 1,
     .fields = (VMStateField[]) {
-        VMSTATE_UINT32(env.cp15.c1_sys.normal, ARMCPU),
+        VMSTATE_UINT32(env.cp15.c2_base0.normal, ARMCPU),
+        VMSTATE_UINT32(env.cp15.c2_base0_hi.normal, ARMCPU),
+        VMSTATE_UINT32(env.cp15.c2_base1.normal, ARMCPU),
+        VMSTATE_UINT32(env.cp15.c2_base1_hi.normal, ARMCPU),
+        VMSTATE_UINT32(env.cp15.c2_control.normal, ARMCPU),
+        VMSTATE_UINT32(env.cp15.c2_mask.normal, ARMCPU),
+        VMSTATE_UINT32(env.cp15.c2_base_mask.normal, ARMCPU),
+        VMSTATE_UINT32(env.cp15.c3.normal, ARMCPU),
+        VMSTATE_UINT32(env.cp15.c5_insn.normal, ARMCPU),
+        VMSTATE_UINT32(env.cp15.c5_data.normal, ARMCPU),
+        VMSTATE_UINT32(env.cp15.c6_insn.normal, ARMCPU),
+        VMSTATE_UINT32(env.cp15.c6_data.normal, ARMCPU),
+        VMSTATE_UINT32(env.cp15.c7_par.normal, ARMCPU),
+        VMSTATE_UINT32(env.cp15.c7_par_hi.normal, ARMCPU),
+        VMSTATE_UINT32(env.cp15.c12_vbar.normal, ARMCPU),
         VMSTATE_UINT32(env.cp15.c13_context.normal, ARMCPU),
         VMSTATE_UINT32(env.cp15.c13_fcse.normal, ARMCPU),
         VMSTATE_UINT32(env.cp15.c13_tls1.normal, ARMCPU),